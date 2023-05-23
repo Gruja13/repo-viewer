f2d54de3c56421b040fa93248bccc032ff0ef370
Djordje Grujovic <djgrujovic13@gmail.com>
2023-05-23 20:53:54 +0200

    Creating authUri callback

--- lib/auth/application/auth_notifier.dart
+++ lib/auth/application/auth_notifier.dart
@@ -1,5 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repo_viewer/auth/domain/auth_faliure.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
 
 part 'auth_notifier.freezed.dart';
 
@@ -9,4 +11,23 @@ class AuthState with _$AuthState {
   const factory AuthState.authenticated() = _Authenticated;
   const factory AuthState.unauthenticated() = _Unauthenticated;
   const factory AuthState.failure(AuthFailure failure) = _Failure;
+}
+
+// Callback function for authorization URL
+typedef AuthUriCallback = Future<Uri> Function(Uri authorizationUrl);
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final GithubAuthenticator _authenticator;
+
+  AuthNotifier(this._authenticator) : super(const AuthState.initial());
+
+  Future<void> checkAndUpdateAuthStatus() async {
+    state = (await _authenticator.isSignedIn())
+        ? const AuthState.authenticated()
+        : const AuthState.unauthenticated();
+  }
+
+  Future<void> signIn(AuthUriCallback authorizationCallback) async {
+    state = const AuthState.authenticated();
+  }
 }
